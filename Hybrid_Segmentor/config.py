700070f62302f02b5b36f72ac3ba25502342c4a1
Anh Thu Be <127480096+ankthuw@users.noreply.github.com>
2025-04-03 03:30:08 +0700

    update dataset

--- Hybrid_Segmentor/config.py
+++ Hybrid_Segmentor/config.py
@@ -4,8 +4,9 @@ import os
 # Huấn luyện từ tập dữ liệu đã copy từ Google Drive
 NUM_EPOCHS = 50
 DATASET_SIZE = {'train' : 4800, 'val' : 600, 'test' : 600}
-# dataset = "/kaggle/working/dataset/split_dataset_6000/"
-dataset = "/kaggle/input/crackvision6000/split_dataset_6000/"
+# dataset = "/kaggle/input/crackvision6000/split_dataset_6000/"
+
+dataset = "/kaggle/input/crackvision12k/split_dataset_final/"
 
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"

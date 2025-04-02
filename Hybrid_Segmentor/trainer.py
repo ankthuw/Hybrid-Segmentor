911d7204c72211e268f820bce2b0578556463646
Anh Thu Be <127480096+ankthuw@users.noreply.github.com>
2025-04-02 23:34:29 +0700

    update trainer

--- Hybrid_Segmentor/trainer.py
+++ Hybrid_Segmentor/trainer.py
@@ -3,23 +3,42 @@ import pytorch_lightning as pl
 import config
 from model import HybridSegmentor
 import torch
-from callback import MyPrintingCallBack, EarlyStopping, checkpoint_callback, early_stopping
+from callback import MyPrintingCallBack, checkpoint_callback, early_stopping
 from pytorch_lightning.loggers import TensorBoardLogger
 from torchsummary import summary
 
 torch.set_float32_matmul_precision("medium")
 
 if __name__ == "__main__":
+    # 1. Logger cho TensorBoard
     logger = TensorBoardLogger("tb_logs", name="v7_BCEDICE_0_2_final")
-    train_loader, val_loader, test_loader = get_loaders(config.TRAIN_IMG_DIR, config.TRAIN_MASK_DIR, config.VAL_IMG_DIR, config.VAL_MASK_DIR, config.TEST_IMG_DIR,
-                                                        config.TEST_MASK_DIR, config.BATCH_SIZE, config.NUM_WORKERS, config.PIN_MEMORY,)   
-    # input_size = (3, 256, 256)
+
+    # 2. Load data
+    train_loader, val_loader, test_loader = get_loaders(
+        config.TRAIN_IMG_DIR, config.TRAIN_MASK_DIR,
+        config.VAL_IMG_DIR, config.VAL_MASK_DIR,
+        config.TEST_IMG_DIR, config.TEST_MASK_DIR,
+        config.BATCH_SIZE, config.NUM_WORKERS, config.PIN_MEMORY
+    )
+
+    # 3. Khởi tạo mô hình
     model = HybridSegmentor(learning_rate=config.LEARNING_RATE).to(config.DEVICE)
-    # print(summary(model, input_size=input_size))
-    
-    trainer = pl.Trainer(logger=logger, accelerator="gpu", min_epochs=1, 
-                         max_epochs=config.NUM_EPOCHS, precision='16-mixed',
-                         callbacks=[checkpoint_callback, early_stopping])
+
+    # 4. Trainer
+    trainer = pl.Trainer(
+        logger=logger,
+        accelerator="gpu" if torch.cuda.is_available() else "cpu",
+        max_epochs=config.NUM_EPOCHS,
+        min_epochs=1,
+        precision="16-mixed",
+        callbacks=[checkpoint_callback, early_stopping, MyPrintingCallBack()]
+    )
+
+    # 5. Train
     trainer.fit(model, train_loader, val_loader)
+
+    # 6. Validate
     trainer.validate(model, val_loader)
+
+    # 7. Test
     trainer.test(model, test_loader, ckpt_path="best")

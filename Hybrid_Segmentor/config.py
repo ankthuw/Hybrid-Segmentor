256305ff989c7f2d8580b3c5b93afe000d7afcd6
Anh Thu Be <127480096+ankthuw@users.noreply.github.com>
2025-04-03 11:02:15 +0700

    Update config.py

--- Hybrid_Segmentor/config.py
+++ Hybrid_Segmentor/config.py
@@ -10,7 +10,7 @@ dataset = "/kaggle/input/crackvision12k/split_dataset_final/"
 
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 4
+BATCH_SIZE = 16
 NUM_WORKERS = 2
 IMAGE_HEIGHT = 256 
 IMAGE_WIDTH = 256
